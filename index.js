c414421200e1e20175c90c21d45a846a2071aa1e
arsenalist777 <arsenalist777@gmail.com>
2024-03-16 19:51:31 +0900

    modify comment

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const OptaStatisticsLoader = require('./loader/opta-statistics-loader.js');
  * main function
  */
 async function main() {
-    // let plStatisticsLoader = new PlStatisticsLoader(process.argv[2]);
-    // plStatisticsLoader.execute();
+    let plStatisticsLoader = new PlStatisticsLoader(process.argv[2]);
+    plStatisticsLoader.execute();
 
-    // let arsenalGoalLogLoader = new ArsenalGoalLogLoader(process.argv[2]);
-    // arsenalGoalLogLoader.execute();
+    let arsenalGoalLogLoader = new ArsenalGoalLogLoader(process.argv[2]);
+    arsenalGoalLogLoader.execute();
 
     let optaStatisticsLoader = new OptaStatisticsLoader(process.argv[2]);
     optaStatisticsLoader.execute();
